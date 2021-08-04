ca0f9502a635fdace8b138312848435d4792a6ab
Sowjanya76 <sowjii7676@gmail.com>
2021-08-04 11:44:27 +0530

    create conflicts

--- src/com/mindtree/bookdistribution/client/gitClass.java
+++ src/com/mindtree/bookdistribution/client/gitClass.java
@@ -2,9 +2,9 @@ package com.mindtree.bookdistribution.client;
 
 public class gitClass {
 	public static void main(String[] args) {
-		 int id=2;
-		  String name="sowji";
-		  System.out.println(id +" "+name);
+		 int id2=2;
+		  String name2="sowji";
+		  System.out.println(id2 +" "+name2);
 	}
   
   
