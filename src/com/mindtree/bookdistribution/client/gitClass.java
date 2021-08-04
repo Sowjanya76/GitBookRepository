a2d9623cb163e75c80c6e27f371723ff3869d724
Sowjanya76 <sowjii7676@gmail.com>
2021-08-04 11:27:22 +0530

    commiting to new branch

---
+++ src/com/mindtree/bookdistribution/client/gitClass.java
@@ -0,0 +1,11 @@
+package com.mindtree.bookdistribution.client;
+
+public class gitClass {
+	public static void main(String[] args) {
+		 int id=2;
+		  String name="sowji";
+		  System.out.println(id +" "+name);
+	}
+  
+  
+}
